b7b10521622fac768df8663d82b1481455cb8962
Jules257 <julesnzejimana@gmail.com>
2021-11-10 16:05:54 +0100

    ajout de location

--- page/Insert.php
+++ page/Insert.php
@@ -4,4 +4,5 @@ $nom=isset($_POST['nom'])?$_POST['nom']:"";
 $prenom=isset($_POST['prenom'])?$_POST['prenom']:"";
 $prep=$jul->prepare("INSERT INTO identite(nom,prenom) values(?,?)");
 $param=array($nom,$prenom);
+header('location:index.php');
 ?>
