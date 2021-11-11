aa5e3d402757c56206dc5cb1877784ab90d2dbd2
Jules257 <julesnzejimana@gmail.com>
2021-11-11 11:58:57 +0100

    ajout des styles et de div de class contain

--- page/index.php
+++ page/index.php
@@ -5,14 +5,26 @@
 	<title>github</title>
 	<meta charset="utf-8">
 </head>
-<body>
-	<h1>paragraphe</h1>
+<body style="color:#ddd"> 
+	<div class="contain">
+		<h1 align="center">paragraphe</h1>
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</div>
+	<style>
+.contain{
+	width:550px; 
+	height:20px;
+	 margin:90px;
+	  padding: 100px;
+	  color: blue;
+}
+</style>
+	
 
 </body>
 </html>
